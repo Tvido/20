e32190a9ccaf4d67aa559a085eb4dab7defd72b8
Volodymyr Viter <mad.tvido@gmail.com>
2022-07-11 21:37:51 +0300

    add schedule

--- assets/js/app.js
+++ assets/js/app.js
@@ -253,3 +253,18 @@ function createBox() {
 }
 
 setInterval(createBox, 400);
+
+// ====================================================================
+const tabHeader = document.getElementsByClassName("tab__header")[0];
+const tabBody = document.getElementsByClassName("tab__body")[0];
+
+const tabsPane = tabHeader.getElementsByTagName("div");
+
+for (let i = 0; i < tabsPane.length; i++) {
+  tabsPane[i].addEventListener("click", function () {
+    tabHeader.getElementsByClassName("active")[0].classList.remove("active");
+    tabsPane[i].classList.add("active");
+    tabBody.getElementsByClassName("active")[0].classList.remove("active");
+    tabBody.getElementsByTagName("div")[i].classList.add("active");
+  });
+}
